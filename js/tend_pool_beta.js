3e6dd2cfe9b6b218755432880d77314bf1fac147
YieldFarming <YieldFarmingInfo@gmail.com>
2020-07-31 20:22:16 -0400

    convert bug fix

--- js/tend_pool_beta.js
+++ js/tend_pool_beta.js
@@ -151,7 +151,41 @@ async function main() {
     };
 
     const convert = async function() {
+        if (!signer)
+            signer = App.provider.getSigner();
 
+        const WEEBTEND_V1_TOKEN = new ethers.Contract(WEEBTEND_TOKEN_ADDR, WEEBTEND_TOKEN_ABI, signer);
+        const WEEBTEND_V2_TOKEN = new ethers.Contract(WEEBTEND_V2_TOKEN_ADDR, WEEBTEND_V2_TOKEN_ABI, signer);
+
+        const currentWeebTENDV1 = await WEEBTEND_V1_TOKEN.balanceOf(App.YOUR_ADDRESS);
+        const allowedWeebTENDV1 = await WEEBTEND_V1_TOKEN.allowance(App.YOUR_ADDRESS, WEEBTEND_V2_TOKEN_ADDR);
+
+        let allow = Promise.resolve();
+
+        if (allowedWeebTENDV1 < currentWeebTENDV1) {
+            showLoading();
+            allow = WEEBTEND_V1_TOKEN.increaseAllowance(WEEBTEND_V2_TOKEN_ADDR, currentWeebTENDV1.sub(allowedWeebTENDV1))
+                .then(function(t) {
+                    return App.provider.waitForTransaction(t.hash);
+                }).catch(function() {
+                    hideLoading();
+                });
+        }
+
+        if (currentWeebTENDV1 > 0) {
+            showLoading();
+            allow.then(async function() {
+                WEEBTEND_V2_TOKEN.convert(currentWeebTENDV1).then(function(t) {
+                    App.provider.waitForTransaction(t.hash).then(function() {
+                        hideLoading();
+                    });
+                }).catch(function() {
+                    hideLoading();
+                });
+            });
+        } else {
+            alert("You have no weebTEND!!");
+        }
     };
 
     if (yourWeebTendV1Amount > 0) {
