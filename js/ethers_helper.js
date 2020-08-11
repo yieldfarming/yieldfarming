4575b693d9d52d018db5473313a434c2e552de4e
YieldFarming <YieldFarmingInfo@gmail.com>
2020-08-11 16:19:52 -0400

    yam test

--- js/ethers_helper.js
+++ js/ethers_helper.js
@@ -302,3 +302,96 @@ const hideLoading = function() {
 }
 
 const toDollar = formatter.format;
+
+const rewardsContract_stake = async function(stakingTokenAddr, rewardPoolAddr, App) {
+
+        const signer = App.provider.getSigner();
+
+        const TEND_TOKEN = new ethers.Contract(stakingTokenAddr, ERC20_ABI, signer);
+        const WEEBTEND_V2_TOKEN = new ethers.Contract(rewardPoolAddr, YFFI_REWARD_CONTRACT_ABI, signer);
+
+        const currentTEND = await TEND_TOKEN.balanceOf(App.YOUR_ADDRESS);
+        const allowedTEND = await TEND_TOKEN.allowance(App.YOUR_ADDRESS, rewardPoolAddr);
+
+        let allow = Promise.resolve();
+
+        if ((allowedTEND / 1e18) < (currentTEND / 1e18)) {
+            showLoading();
+            allow = TEND_TOKEN.approve(rewardPoolAddr, currentTEND * 1e10)
+                .then(function(t) {
+                    return App.provider.waitForTransaction(t.hash);
+                }).catch(function() {
+                    hideLoading();
+                });
+        }
+
+        if ((currentTEND / 1e18) > 0) {
+            showLoading();
+            allow.then(async function() {
+                WEEBTEND_V2_TOKEN.stake(currentTEND).then(function(t) {
+                    App.provider.waitForTransaction(t.hash).then(function() {
+                        hideLoading();
+                    });
+                }).catch(function() {
+                    hideLoading();
+                });
+            });
+        } else {
+            alert("You have no TEND!!");
+        }
+};
+
+const rewardsContract_unstake = async function(rewardPoolAddr, App) {
+    const signer = App.provider.getSigner();
+
+    const REWARD_POOL = new ethers.Contract(rewardPoolAddr, Y_STAKING_POOL_ABI, signer);
+    const currentStakedAmount = await REWARD_POOL.balanceOf(App.YOUR_ADDRESS);
+    const earnedYFFI = (await REWARD_POOL.earned(App.YOUR_ADDRESS)) / 1e18;
+
+    if (earnedYFFI > 0) {
+        showLoading();
+        REWARD_POOL.withdraw(currentStakedAmount)
+            .then(function(t) {
+                return App.provider.waitForTransaction(t.hash);
+            }).catch(function() {
+            hideLoading();
+        });
+    }
+};
+
+const rewardsContract_exit = async function(rewardPoolAddr, App) {
+    const signer = App.provider.getSigner();
+
+    const REWARD_POOL = new ethers.Contract(rewardPoolAddr, Y_STAKING_POOL_ABI, signer);
+    const currentStakedAmount = (await REWARD_POOL.balanceOf(App.YOUR_ADDRESS)) / 1e18;
+
+    if (currentStakedAmount > 0) {
+        showLoading();
+        REWARD_POOL.exit()
+            .then(function(t) {
+                return App.provider.waitForTransaction(t.hash);
+            }).catch(function() {
+            hideLoading();
+        });
+    }
+};
+
+const rewardsContract_claim = async function(rewardPoolAddr, App) {
+    const signer = App.provider.getSigner();
+
+    const WEEBTEND_V2_TOKEN = new ethers.Contract(rewardPoolAddr, Y_STAKING_POOL_ABI, signer);
+
+    console.log(App.YOUR_ADDRESS);
+
+    const earnedYFFI = (await WEEBTEND_V2_TOKEN.earned(App.YOUR_ADDRESS)) / 1e18;
+
+    if (earnedYFFI > 0) {
+        showLoading();
+        WEEBTEND_V2_TOKEN.getReward({gasLimit: 200000})
+            .then(function(t) {
+                return App.provider.waitForTransaction(t.hash);
+            }).catch(function() {
+            hideLoading();
+        });
+    }
+};
