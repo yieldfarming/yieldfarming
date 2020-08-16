9870fef7745de30e423b9bb225342cb7e90ae725
YieldFarming <YieldFarmingInfo@gmail.com>
2020-08-16 17:06:52 -0400

    add dist folder

---
+++ dist/js/homepage.f656300b6682d01ad6f87e1916140cae.js
@@ -0,0 +1,12 @@
+/*!
+* Static Boilerplate EJS-SASS
+* Boilerplate for a Static website using EJS and SASS
+* example.com
+* @author Brenton Cozby -- https://yieldfarming.info
+* Copyright 2020. MIT Licensed.
+*/
+
+$(async function() {
+
+});
+
