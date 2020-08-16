5fae2cc5ff6108fc74f46bd2a6f787373d9a6fb9
YieldFarming <YieldFarmingInfo@gmail.com>
2020-08-16 16:12:19 -0400

    Move to ejs and node

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+/** Variables available in all js files:
+ * all the exported constants from globals.js
+ */
+
+/** Directories available as aliases
+ * all the paths within Dir in globals.js
+ */
+import $ from "jquery";
+import { ethers } from "ethers";
+
+import dompurify from 'dompurify'
+
+import 'picturefill'
+import 'utils/errors'
+import 'utils/validation'
+
+window.$ = $;
+window.ethers = ethers;
+window.asciichart = require("asciichart");
+
+// eslint-disable-next-line no-console
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`)
