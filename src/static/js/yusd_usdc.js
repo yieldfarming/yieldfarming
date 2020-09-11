4022a310c780f4aaf308f52149ef59093c4bad5c
John Lim <penandlim@gmail.com>
2020-09-10 20:18:39 -0400

    yusd oct20

--- src/static/js/yusd_usdc.js
+++ src/static/js/yusd_usdc.js
@@ -16,11 +16,11 @@ async function main() {
     const totalBPTAmount = await YUSD_USDC_BALANCER_POOL.totalSupply() / 1e18;
     const yourBPTAmount = await YUSD_USDC_BPT_TOKEN.balanceOf(App.YOUR_ADDRESS) / 1e18;
 
-    const totalYUSDSEP20Amount = await YUSD_USDC_BALANCER_POOL.getBalance(YUSDSEP20_TOKEN_ADDR) / 1e18;
+    const totalYUSDOCT20Amount = await YUSD_USDC_BALANCER_POOL.getBalance(YUSDOCT20_TOKEN_ADDR) / 1e18;
     const totalUSDCAmount = await YUSD_USDC_BALANCER_POOL.getBalance(USDC_TOKEN_ADDR) / 1e6;
 
 
-    const YUSDSEP20PerBPT = totalYUSDSEP20Amount / totalBPTAmount;
+    const YUSDOCT20PerBPT = totalYUSDOCT20Amount / totalBPTAmount;
     const USDCPerBPT = totalUSDCAmount / totalBPTAmount;
 
     // Find out reward rate
@@ -33,24 +33,24 @@ async function main() {
     const prices = await lookUpPrices(["usd-coin", "uma"]);
     const USDCPrice = prices["usd-coin"].usd;
     const UMAPrice = prices["uma"].usd;
-    const YUSDSEP20Price = (await YUSD_USDC_BALANCER_POOL.getSpotPrice(USDC_TOKEN_ADDR, YUSDSEP20_TOKEN_ADDR) /1e6) * USDCPrice;
+    const YUSDOCT20Price = (await YUSD_USDC_BALANCER_POOL.getSpotPrice(USDC_TOKEN_ADDR, YUSDOCT20_TOKEN_ADDR) /1e6) * USDCPrice;
 
-    const BPTPrice = YUSDSEP20PerBPT * YUSDSEP20Price + USDCPerBPT * USDCPrice;
+    const BPTPrice = YUSDOCT20PerBPT * YUSDOCT20Price + USDCPerBPT * USDCPrice;
 
     // Finished. Start printing
 
     _print("========== PRICES ==========")
     _print(`1 UMA         = $${UMAPrice}`);
-    _print(`1 yUSD-SEP20  = $${YUSDSEP20Price}`);
+    _print(`1 yUSD-OCT20  = $${YUSDOCT20Price}`);
     _print(`1 USDC        = $${USDCPrice}\n`);
-    _print(`1 BPT         = [${YUSDSEP20PerBPT} yUSD-SEP20, ${USDCPerBPT} USDC]`);
+    _print(`1 BPT         = [${YUSDOCT20PerBPT} yUSD-SEP20, ${USDCPerBPT} USDC]`);
     _print(`              = ${toDollar(BPTPrice)}\n`);
 
-    _print("========== STAKING =========")
+    _print("========== HOLDING =========")
     _print(`There are total   : ${totalBPTAmount} BPT issued by yUSD-USDC Balancer Pool.`);
     _print(`                  = ${toDollar(totalBPTAmount * BPTPrice)}\n`);
     _print(`You are holding   : ${yourBPTAmount} BPT (${toFixed(yourBPTAmount * 100 / totalBPTAmount, 3)}% of the pool)`);
-    _print(`                  = [${YUSDSEP20PerBPT * yourBPTAmount} yUSD, ${USDCPerBPT * yourBPTAmount} USDC]`);
+    _print(`                  = [${YUSDOCT20PerBPT * yourBPTAmount} yUSD, ${USDCPerBPT * yourBPTAmount} USDC]`);
     _print(`                  = ${toDollar(yourBPTAmount * BPTPrice)}\n`);
 
     // UMA REWARDS
